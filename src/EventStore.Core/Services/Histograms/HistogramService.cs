fe3d695fb0c0700f164b45f6b56fa5947ca1dd22
Seabiscuit <cuteant@outlook.com>
2017-04-11 17:01:50 +0800

    加入 HdrHistogram 源代码，所有项目加强签名

--- src/EventStore.Core/Services/Histograms/HistogramService.cs
+++ src/EventStore.Core/Services/Histograms/HistogramService.cs
@@ -4,114 +4,110 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
-using HdrHistogram.NET;
+using HdrHistogram;
 
 namespace EventStore.Core.Services.Histograms
 {
-    //histogram service is just a static class used by everyone else if histograms are enabled.
-    public static class HistogramService
+  //histogram service is just a static class used by everyone else if histograms are enabled.
+  public static class HistogramService
+  {
+    private const long NUMBEROFNS = 1000000000L;
+    private static readonly Stopwatch _stopwatch = new Stopwatch();
+    private static readonly ConcurrentDictionary<string, LongHistogram> Histograms = new ConcurrentDictionary<string, LongHistogram>();
+
+    static HistogramService()
     {
-        private const long NUMBEROFNS = 1000000000L;
-        private static readonly Stopwatch _stopwatch = new Stopwatch();
-        private static readonly ConcurrentDictionary<string, Histogram> Histograms = new ConcurrentDictionary<string, Histogram>();
-
-        static HistogramService()
-        {
-            _stopwatch.Start();
-        }
-
-        public static Histogram GetHistogram(string name)
-        {
-            Histogram ret;
-            Histograms.TryGetValue(name, out ret);
-            return ret;
-        }
-
-        public static Measurement Measure(string name)
-        {
-            var histogram = GetHistogram(name);
-            return new Measurement {watch = _stopwatch, Start = _stopwatch.ElapsedTicks, Histogram=histogram};
-        }
-
-        public static void SetValue(string name, long value)
-        {
-            if (value >= NUMBEROFNS) return;
-            if(name == null) return;
-            Histogram hist;
-            if(!Histograms.TryGetValue(name, out hist)) {
-                return;
-            }
-            lock (hist)
-            {
-                hist.recordValue(value);
-            }
-        }
-
-        public static void CreateHistogram(string name)
-        {
-            Histograms.TryAdd(name, new Histogram(NUMBEROFNS, 3));
-        }
-
-        public static void CreateHistograms()
-        {
-            CreateHistogram("writer-flush");
-            CreateHistogram("chaser-wait");
-            CreateHistogram("chaser-flush");
-            CreateHistogram("reader-readevent");
-            CreateHistogram("reader-streamrange");
-            CreateHistogram("reader-allrange");
-            CreateHistogram("request-manager");
-            CreateHistogram("tcp-send");
-            CreateHistogram("http-send");
-        }
-
-        public static void StartJitterMonitor()
-        {
-            CreateHistogram("jitter");
-            Task.Factory.StartNew(x =>
-            {
-                var watch = new Stopwatch();
-                watch.Start();
-                while (true)
-                {
-                    using (Measure("jitter"))
-                    {
-                        Thread.Sleep(1);
-                    }
-                }
-            }, null, TaskCreationOptions.LongRunning);
-        }
+      _stopwatch.Start();
     }
 
+    public static LongHistogram GetHistogram(string name)
+    {
+      Histograms.TryGetValue(name, out LongHistogram ret);
+      return ret;
+    }
 
-        public struct Measurement : IDisposable
+    public static Measurement Measure(string name)
+    {
+      var histogram = GetHistogram(name);
+      return new Measurement { watch = _stopwatch, Start = _stopwatch.ElapsedTicks, Histogram = histogram };
+    }
+
+    public static void SetValue(string name, long value)
+    {
+      if (value >= NUMBEROFNS) return;
+      if (name == null) return;
+      if (!Histograms.TryGetValue(name, out LongHistogram hist)) { return; }
+      lock (hist)
+      {
+        hist.RecordValue(value);
+      }
+    }
+
+    public static void CreateHistogram(string name)
+    {
+      Histograms.TryAdd(name, new LongHistogram(NUMBEROFNS, 3));
+    }
+
+    public static void CreateHistograms()
+    {
+      CreateHistogram("writer-flush");
+      CreateHistogram("chaser-wait");
+      CreateHistogram("chaser-flush");
+      CreateHistogram("reader-readevent");
+      CreateHistogram("reader-streamrange");
+      CreateHistogram("reader-allrange");
+      CreateHistogram("request-manager");
+      CreateHistogram("tcp-send");
+      CreateHistogram("http-send");
+    }
+
+    public static void StartJitterMonitor()
+    {
+      CreateHistogram("jitter");
+      Task.Factory.StartNew(x =>
+      {
+        var watch = new Stopwatch();
+        watch.Start();
+        while (true)
         {
-            public Stopwatch watch;
-            public Histogram Histogram;
-            public long Start;
-
-            public void Dispose()
-            {
-                if (Histogram == null) return;
-                lock (Histogram)
-                {
-                    var valueToRecord = (((double)watch.ElapsedTicks - Start) / Stopwatch.Frequency) * 1000000000;
-                    if (valueToRecord < HighestPowerOf2(Histogram.getHighestTrackableValue()))
-                    {
-                        Histogram.recordValue((long)valueToRecord);
-                    }
-                }
-            }
-            private static long HighestPowerOf2(long x)
-            {
-                x--;
-                x |= (x >> 1);
-                x |= (x >> 2);
-                x |= (x >> 4);
-                x |= (x >> 8);
-                x |= (x >> 16);
-                return (x + 1);
-            }
-
+          using (Measure("jitter"))
+          {
+            Thread.Sleep(1);
+          }
         }
+      }, null, TaskCreationOptions.LongRunning);
+    }
+  }
+
+
+  public struct Measurement : IDisposable
+  {
+    public Stopwatch watch;
+    public LongHistogram Histogram;
+    public long Start;
+
+    public void Dispose()
+    {
+      if (Histogram == null) return;
+      lock (Histogram)
+      {
+        var valueToRecord = (((double)watch.ElapsedTicks - Start) / Stopwatch.Frequency) * 1000000000;
+        if (valueToRecord < HighestPowerOf2(Histogram.HighestTrackableValue))
+        {
+          Histogram.RecordValue((long)valueToRecord);
+        }
+      }
+    }
+    private static long HighestPowerOf2(long x)
+    {
+      x--;
+      x |= (x >> 1);
+      x |= (x >> 2);
+      x |= (x >> 4);
+      x |= (x >> 8);
+      x |= (x >> 16);
+      return (x + 1);
+    }
+
+  }
 }
--- src/EventStore.Core/Services/Transport/Http/Controllers/HistogramController.cs
+++ src/EventStore.Core/Services/Transport/Http/Controllers/HistogramController.cs
@@ -5,42 +5,43 @@ using EventStore.Common.Utils;
 using EventStore.Transport.Http;
 using EventStore.Transport.Http.Codecs;
 using EventStore.Transport.Http.EntityManagement;
+using HdrHistogram;
 
 namespace EventStore.Core.Services.Transport.Http.Controllers
 {
-    public class HistogramController : IHttpController
+  public class HistogramController : IHttpController
+  {
+    private static readonly ICodec[] SupportedCodecs = new ICodec[] { Codec.Json, Codec.Xml, Codec.ApplicationXml, Codec.Text };
+
+    public void Subscribe(IHttpService service)
     {
-        private static readonly ICodec[] SupportedCodecs = new ICodec[] { Codec.Json, Codec.Xml, Codec.ApplicationXml, Codec.Text };
-
-        public void Subscribe(IHttpService service)
-        {
-            Ensure.NotNull(service, "service");
-            service.RegisterAction(new ControllerAction("/histogram/{name}", HttpMethod.Get, Codec.NoCodecs, SupportedCodecs), OnGetHistogram);
-        }
-
-        private void OnGetHistogram(HttpEntityManager entity, UriTemplateMatch match)
-        {
-            var name = match.BoundVariables["name"];
-           
-            var histogram = Histograms.HistogramService.GetHistogram(name);
-            if (histogram == null)
-            {
-                entity.ReplyStatus(HttpStatusCode.NotFound, "Not found", _ => { });
-                return;
-            }
-            var writer = new StringWriter();
-            lock (histogram)
-            {
-                histogram.outputPercentileDistribution(writer, outputValueUnitScalingRatio: 1000.0*1000.0);
-            }
-            var response = Encoding.ASCII.GetBytes(writer.ToString());
-            entity.Reply(response,
-                HttpStatusCode.OK, 
-                "OK", 
-                ContentType.PlainText, 
-                Encoding.ASCII, 
-                null,
-                _ => { });
-        }
+      Ensure.NotNull(service, nameof(service));
+      service.RegisterAction(new ControllerAction("/histogram/{name}", HttpMethod.Get, Codec.NoCodecs, SupportedCodecs), OnGetHistogram);
     }
+
+    private void OnGetHistogram(HttpEntityManager entity, UriTemplateMatch match)
+    {
+      var name = match.BoundVariables["name"];
+
+      var histogram = Histograms.HistogramService.GetHistogram(name);
+      if (histogram == null)
+      {
+        entity.ReplyStatus(HttpStatusCode.NotFound, "Not found", _ => { });
+        return;
+      }
+      var writer = new StringWriter();
+      lock (histogram)
+      {
+        histogram.OutputPercentileDistribution(writer, outputValueUnitScalingRatio: 1000.0 * 1000.0);
+      }
+      var response = Encoding.ASCII.GetBytes(writer.ToString());
+      entity.Reply(response,
+          HttpStatusCode.OK,
+          "OK",
+          ContentType.PlainText,
+          Encoding.ASCII,
+          null,
+          _ => { });
+    }
+  }
 }
