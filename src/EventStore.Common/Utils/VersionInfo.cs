30ae5d422033b746eb8f512ae2a37eb32e1b6081
seabiscuit <cuteant@outlook.com>
2017-04-28 04:00:57 +0800

    update nlog => 4.4.7

--- src/EventStore.Common/Utils/VersionInfo.cs
+++ src/EventStore.Common/Utils/VersionInfo.cs
@@ -3,9 +3,9 @@
   /// <summary>These are patched by the build process! Do not edit them manually.</summary>
   public static class VersionInfo
   {
-    public static readonly string Version = "4.0.0.8";
-    public static readonly string Branch = "release-v4.0.1";
-    public static readonly string Hashtag = "3d0a6a8670c51a2e1c52f3ec3467f9417247d739";
-    public static readonly string Timestamp = "Fri, 21 Apr 2017 20:08:08 +0800";
+    public static readonly string Version = "4.0.2.168";
+    public static readonly string Branch = "release-v4.0.2";
+    public static readonly string Hashtag = "a45c296a3b09555e0064a6c6cdfca55e1c9cfc5e";
+    public static readonly string Timestamp = "Fri, 28 Apr 2017 20:08:08 +0800";
   }
 }
