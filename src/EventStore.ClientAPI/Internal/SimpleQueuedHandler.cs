92c7041a89c7c60d6b0bc0297e7de09310bf9a74
seabiscuit <cuteant@outlook.com>
2017-05-19 01:24:41 +0800

    AsynchQueueAgent

--- src/EventStore.ClientAPI/Internal/SimpleQueuedHandler.cs
+++ src/EventStore.ClientAPI/Internal/SimpleQueuedHandler.cs
@@ -1,26 +1,18 @@
 ﻿using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Threading;
 using CuteAnt.Runtime;
 using EventStore.ClientAPI.Common.Utils;
 using Microsoft.Extensions.Logging;
 
 namespace EventStore.ClientAPI.Internal
 {
-  internal sealed class SimpleQueuedHandler : AsynchAgent, IDisposable
+  internal sealed class SimpleQueuedHandler : AsynchQueueAgent<Message>, IDisposable
   {
     private readonly Dictionary<Type, Action<Message>> _handlers;
-    private BlockingCollection<Message> _messageQueue;
-
-    public int Count => _messageQueue != null ? _messageQueue.Count : 0;
-
-    public bool IsCompleted => _messageQueue != null ? _messageQueue.IsCompleted : true;
 
     public SimpleQueuedHandler()
     {
       _handlers = new Dictionary<Type, Action<Message>>();
-      _messageQueue = new BlockingCollection<Message>();
     }
 
     public void RegisterHandler<T>(Action<T> handler) where T : Message
@@ -30,55 +22,23 @@ namespace EventStore.ClientAPI.Internal
       _handlers.Add(typeof(T), msg => handler((T)msg));
     }
 
-    public void EnqueueMessage(Message message)
+    protected override void Process(Message message)
     {
-      if (null == _messageQueue) { return; }
-      if (null == message) { throw new ArgumentNullException(nameof(message)); }
-      _messageQueue.Add(message);
-    }
-
-    protected sealed override void Run()
-    {
-      while (true)
+      if (_handlers.TryGetValue(message.GetType(), out Action<Message> handler))
       {
-        if (null == Cts || Cts.IsCancellationRequested) { return; }
-        Message message;
-        try
-        {
-          message = _messageQueue.Take();
-        }
-        catch (InvalidOperationException)
-        {
-          Log.LogWarning("Stop message processed");
-          break;
-        }
-
-        if (_handlers.TryGetValue(message.GetType(), out Action<Message> handler))
-        {
-          handler.Invoke(message);
-        }
-        else
-        {
-          var errorMsg = $"No handler registered for message {message.GetType().Name}";
-          Log.LogCritical(errorMsg);
-          throw new Exception(errorMsg);
-        }
+        handler.Invoke(message);
+      }
+      else
+      {
+        var errorMsg = $"No handler registered for message {message.GetType().Name}";
+        Log.LogCritical(errorMsg);
+        throw new Exception(errorMsg);
       }
     }
 
-    public sealed override void Stop()
+    protected override void ProcessBatch(List<Message> messages)
     {
-      if (_messageQueue != null)
-      {
-        _messageQueue.CompleteAdding();
-        var spinWait = new SpinWait();
-        while (true)
-        {
-          spinWait.SpinOnce();
-          if (this.IsCompleted) { break; }
-        }
-      }
-      base.Stop();
+      throw new NotImplementedException();
     }
 
     #region IDisposable Members
@@ -87,12 +47,9 @@ namespace EventStore.ClientAPI.Internal
     {
       if (!disposing) return;
 
+      Stop(dropPendingMessages: false);
+
       base.Dispose(disposing);
-
-      Stop();
-
-      _messageQueue?.Dispose();
-      _messageQueue = null;
     }
 
     #endregion
