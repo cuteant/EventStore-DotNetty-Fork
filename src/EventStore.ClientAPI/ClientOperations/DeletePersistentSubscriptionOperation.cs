12204ed06384feaacc97e27182f8d4a4e27784cf
Seabiscuit <cuteant@outlook.com>
2017-05-27 19:04:59 +0800

    AlreadyExists => DoesNotExist

--- src/EventStore.ClientAPI/ClientOperations/DeletePersistentSubscriptionOperation.cs
+++ src/EventStore.ClientAPI/ClientOperations/DeletePersistentSubscriptionOperation.cs
@@ -4,61 +4,57 @@ using EventStore.ClientAPI.Exceptions;
 using EventStore.ClientAPI.Internal;
 using EventStore.ClientAPI.Messages;
 using EventStore.ClientAPI.SystemData;
-using Microsoft.Extensions.Logging;
 
 namespace EventStore.ClientAPI.ClientOperations
 {
-    internal class DeletePersistentSubscriptionOperation : OperationBase<PersistentSubscriptionDeleteResult, ClientMessage.DeletePersistentSubscriptionCompleted>
+  internal class DeletePersistentSubscriptionOperation : OperationBase<PersistentSubscriptionDeleteResult, ClientMessage.DeletePersistentSubscriptionCompleted>
+  {
+    private readonly string _stream;
+    private readonly string _groupName;
+
+    public DeletePersistentSubscriptionOperation(TaskCompletionSource<PersistentSubscriptionDeleteResult> source,
+      string stream, string groupName, UserCredentials userCredentials)
+      : base(source, TcpCommand.DeletePersistentSubscription, TcpCommand.DeletePersistentSubscriptionCompleted, userCredentials)
     {
-        private readonly string _stream;
-        private readonly string _groupName;
-
-        public DeletePersistentSubscriptionOperation(
-                                       TaskCompletionSource<PersistentSubscriptionDeleteResult> source,
-                                       string stream,
-                                       string groupName,
-                                       UserCredentials userCredentials)
-            : base(source, TcpCommand.DeletePersistentSubscription, TcpCommand.DeletePersistentSubscriptionCompleted, userCredentials)
-        {
-            _stream = stream;
-            _groupName = groupName;
-        }
-
-        protected override object CreateRequestDto()
-        {
-            return new ClientMessage.DeletePersistentSubscription(_groupName, _stream);
-        }
-
-        protected override InspectionResult InspectResponse(ClientMessage.DeletePersistentSubscriptionCompleted response)
-        {
-            switch (response.Result)
-            {
-                case ClientMessage.DeletePersistentSubscriptionCompleted.DeletePersistentSubscriptionResult.Success:
-                    Succeed();
-                    return new InspectionResult(InspectionDecision.EndOperation, "Success");
-                case ClientMessage.DeletePersistentSubscriptionCompleted.DeletePersistentSubscriptionResult.Fail:
-                    Fail(new InvalidOperationException(String.Format("Subscription group {0} on stream {1} failed '{2}'", _groupName, _stream, response.Reason)));
-                    return new InspectionResult(InspectionDecision.EndOperation, "Fail");
-                case ClientMessage.DeletePersistentSubscriptionCompleted.DeletePersistentSubscriptionResult.AccessDenied:
-                    Fail(new AccessDeniedException(string.Format("Write access denied for stream '{0}'.", _stream)));
-                    return new InspectionResult(InspectionDecision.EndOperation, "AccessDenied");
-                case ClientMessage.DeletePersistentSubscriptionCompleted.DeletePersistentSubscriptionResult.DoesNotExist:
-                    Fail(new InvalidOperationException(String.Format("Subscription group {0} on stream {1} does not exist", _groupName, _stream)));
-                    return new InspectionResult(InspectionDecision.EndOperation, "AlreadyExists");
-                default:
-                    throw new Exception(string.Format("Unexpected OperationResult: {0}.", response.Result));
-            }
-        }
-
-        protected override PersistentSubscriptionDeleteResult TransformResponse(ClientMessage.DeletePersistentSubscriptionCompleted response)
-        {
-            return new PersistentSubscriptionDeleteResult(PersistentSubscriptionDeleteStatus.Success);
-        }
-
-        
-        public override string ToString()
-        {
-            return string.Format("Stream: {0}, Group Name: {1}", _stream, _groupName);
-        }
+      _stream = stream;
+      _groupName = groupName;
     }
+
+    protected override object CreateRequestDto()
+    {
+      return new ClientMessage.DeletePersistentSubscription(_groupName, _stream);
+    }
+
+    protected override InspectionResult InspectResponse(ClientMessage.DeletePersistentSubscriptionCompleted response)
+    {
+      switch (response.Result)
+      {
+        case ClientMessage.DeletePersistentSubscriptionCompleted.DeletePersistentSubscriptionResult.Success:
+          Succeed();
+          return new InspectionResult(InspectionDecision.EndOperation, "Success");
+        case ClientMessage.DeletePersistentSubscriptionCompleted.DeletePersistentSubscriptionResult.Fail:
+          Fail(new InvalidOperationException($"Subscription group {_groupName} on stream {_stream} failed '{response.Reason}'"));
+          return new InspectionResult(InspectionDecision.EndOperation, "Fail");
+        case ClientMessage.DeletePersistentSubscriptionCompleted.DeletePersistentSubscriptionResult.AccessDenied:
+          Fail(new AccessDeniedException($"Write access denied for stream '{_stream}'."));
+          return new InspectionResult(InspectionDecision.EndOperation, "AccessDenied");
+        case ClientMessage.DeletePersistentSubscriptionCompleted.DeletePersistentSubscriptionResult.DoesNotExist:
+          Fail(new InvalidOperationException($"Subscription group {_groupName} on stream {_stream} does not exist"));
+          return new InspectionResult(InspectionDecision.EndOperation, "DoesNotExist");
+        default:
+          throw new Exception($"Unexpected OperationResult: {response.Result}.");
+      }
+    }
+
+    protected override PersistentSubscriptionDeleteResult TransformResponse(ClientMessage.DeletePersistentSubscriptionCompleted response)
+    {
+      return new PersistentSubscriptionDeleteResult(PersistentSubscriptionDeleteStatus.Success);
+    }
+
+
+    public override string ToString()
+    {
+      return $"Stream: {_stream}, Group Name: {_groupName}";
+    }
+  }
 }
