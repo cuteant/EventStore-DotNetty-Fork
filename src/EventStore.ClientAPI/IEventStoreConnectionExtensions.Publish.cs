8c2dffee5b9d3588c000f3c26b7bb1a35af44f93
seabiscuit <cuteant@outlook.com>
2017-06-09 01:55:37 +0800

    using transaction

--- src/EventStore.ClientAPI/IEventStoreConnectionExtensions.Publish.cs
+++ src/EventStore.ClientAPI/IEventStoreConnectionExtensions.Publish.cs
@@ -86,12 +86,12 @@ namespace EventStore.ClientAPI
       Dictionary<string, object> eventContext = null, Type expectedType = null, UserCredentials userCredentials = null)
       where TEvent : class
     {
-      if (null == connection) { throw new ArgumentNullException(nameof(connection)); }
-
       var actualType = typeof(TEvent);
       if (actualType == TypeHelper.ObjectType)
       {
+        if (null == connection) { throw new ArgumentNullException(nameof(connection)); }
         if (null == events) { throw new ArgumentNullException(nameof(events)); }
+
         WriteResult result = default(WriteResult);
         foreach (var @event in events)
         {
@@ -128,11 +128,10 @@ namespace EventStore.ClientAPI
       IList<Dictionary<string, object>> eventContexts, Type expectedType = null, UserCredentials userCredentials = null)
       where TEvent : class
     {
-      if (null == connection) { throw new ArgumentNullException(nameof(connection)); }
-
       var actualType = typeof(TEvent);
       if (actualType == TypeHelper.ObjectType)
       {
+        if (null == connection) { throw new ArgumentNullException(nameof(connection)); }
         if (null == events) { throw new ArgumentNullException(nameof(events)); }
         if (null == eventContexts) { throw new ArgumentNullException(nameof(eventContexts)); }
         if (events.Count != eventContexts.Count) { throw new ArgumentOutOfRangeException(nameof(eventContexts)); }
@@ -173,12 +172,12 @@ namespace EventStore.ClientAPI
       Dictionary<string, object> eventContext = null, Type expectedType = null, UserCredentials userCredentials = null)
       where TEvent : class
     {
-      if (null == connection) { throw new ArgumentNullException(nameof(connection)); }
-
       var actualType = typeof(TEvent);
       if (actualType == TypeHelper.ObjectType)
       {
+        if (null == connection) { throw new ArgumentNullException(nameof(connection)); }
         if (null == events) { throw new ArgumentNullException(nameof(events)); }
+
         WriteResult result = default(WriteResult);
         foreach (var @event in events)
         {
@@ -215,11 +214,10 @@ namespace EventStore.ClientAPI
       IList<Dictionary<string, object>> eventContexts, Type expectedType = null, UserCredentials userCredentials = null)
       where TEvent : class
     {
-      if (null == connection) { throw new ArgumentNullException(nameof(connection)); }
-
       var actualType = typeof(TEvent);
       if (actualType == TypeHelper.ObjectType)
       {
+        if (null == connection) { throw new ArgumentNullException(nameof(connection)); }
         if (null == events) { throw new ArgumentNullException(nameof(events)); }
         if (null == eventContexts) { throw new ArgumentNullException(nameof(eventContexts)); }
         if (events.Count != eventContexts.Count) { throw new ArgumentOutOfRangeException(nameof(eventContexts)); }
@@ -251,6 +249,219 @@ namespace EventStore.ClientAPI
         return connection.AppendToStreamAsync(CombineStreamId(streamAttr.StreamId, topic), streamAttr.ExpectedVersion, eventDatas, userCredentials);
       }
       return connection.AppendToStreamAsync(CombineStreamId(JsonConvertX.SerializeTypeName(expectedType ?? actualType), topic), ExpectedVersion.Any, eventDatas, userCredentials);
+    }
+
+    #endregion
+
+    #region -- PublishEventsAsync Using Transaction --
+
+    public static async Task<WriteResult> PublishEventsAsync<TEvent>(this IEventStoreConnectionBase connection, int batchSize, ICollection<TEvent> events,
+      Dictionary<string, object> eventContext = null, Type expectedType = null, UserCredentials userCredentials = null)
+      where TEvent : class
+    {
+      var actualType = typeof(TEvent);
+      if (actualType == TypeHelper.ObjectType)
+      {
+        if (null == connection) { throw new ArgumentNullException(nameof(connection)); }
+        if (null == events) { throw new ArgumentNullException(nameof(events)); }
+
+        // 无法确定所发布事件是否在同一 stream 中，不能采用事物
+        WriteResult result = default(WriteResult);
+        foreach (var @event in events)
+        {
+          result = await PublishEventAsync(connection, @event.GetType(), @event, eventContext, expectedType, userCredentials).ConfigureAwait(false);
+        }
+        return result;
+      }
+
+      return await PublishEventsAsync(connection, actualType, batchSize, events, eventContext, expectedType, userCredentials).ConfigureAwait(false);
+    }
+    public static async Task<WriteResult> PublishEventsAsync<TEvent>(this IEventStoreConnectionBase connection, Type actualType, int batchSize, ICollection<TEvent> events,
+      Dictionary<string, object> eventContext = null, Type expectedType = null, UserCredentials userCredentials = null)
+      where TEvent : class
+    {
+      if (batchSize <= 0) { throw new ArgumentOutOfRangeException(nameof(batchSize)); }
+      if (null == events) { throw new ArgumentNullException(nameof(events)); }
+
+      if (events.Count <= batchSize)
+      {
+        return await PublishEventsAsync(connection, actualType, events, eventContext, expectedType, userCredentials).ConfigureAwait(false);
+      }
+
+      if (null == connection) { throw new ArgumentNullException(nameof(connection)); }
+      if (null == actualType) { throw new ArgumentNullException(nameof(actualType)); }
+
+      var streamAttr = SerializationManager.GetStreamProvider(actualType, expectedType);
+      var eventDatas = SerializationManager.SerializeEvents(streamAttr, actualType, events, eventContext, expectedType);
+      if (streamAttr != null)
+      {
+        return await DoWriteAsync(connection, streamAttr.StreamId, streamAttr.ExpectedVersion, eventDatas, batchSize, userCredentials).ConfigureAwait(false);
+      }
+      else
+      {
+        return await DoWriteAsync(connection, JsonConvertX.SerializeTypeName(expectedType ?? actualType), ExpectedVersion.Any, eventDatas, batchSize, userCredentials).ConfigureAwait(false);
+      }
+    }
+
+
+
+
+    public static async Task<WriteResult> PublishEventsAsync<TEvent>(this IEventStoreConnectionBase connection, int batchSize, IList<TEvent> events,
+      IList<Dictionary<string, object>> eventContexts, Type expectedType = null, UserCredentials userCredentials = null)
+      where TEvent : class
+    {
+      var actualType = typeof(TEvent);
+      if (actualType == TypeHelper.ObjectType)
+      {
+        if (null == connection) { throw new ArgumentNullException(nameof(connection)); }
+        if (null == events) { throw new ArgumentNullException(nameof(events)); }
+        if (null == eventContexts) { throw new ArgumentNullException(nameof(eventContexts)); }
+        if (events.Count != eventContexts.Count) { throw new ArgumentOutOfRangeException(nameof(eventContexts)); }
+
+        // 无法确定所发布事件是否在同一 stream 中，不能采用事物
+        WriteResult result = default(WriteResult);
+        for (var idx = 0; idx < events.Count; idx++)
+        {
+          var @event = events[idx];
+          result = await PublishEventAsync(connection, @event.GetType(), @event, eventContexts[idx], expectedType, userCredentials).ConfigureAwait(false);
+        }
+        return result;
+      }
+      return await PublishEventsAsync(connection, actualType, batchSize, events, eventContexts, expectedType, userCredentials).ConfigureAwait(false);
+    }
+    public static async Task<WriteResult> PublishEventsAsync<TEvent>(this IEventStoreConnectionBase connection, Type actualType, int batchSize, IList<TEvent> events,
+      IList<Dictionary<string, object>> eventContexts, Type expectedType = null, UserCredentials userCredentials = null)
+      where TEvent : class
+    {
+      if (batchSize <= 0) { throw new ArgumentOutOfRangeException(nameof(batchSize)); }
+      if (null == events) { throw new ArgumentNullException(nameof(events)); }
+
+      if (events.Count <= batchSize)
+      {
+        return await PublishEventsAsync(connection, actualType, events, eventContexts, expectedType, userCredentials).ConfigureAwait(false);
+      }
+
+      if (null == connection) { throw new ArgumentNullException(nameof(connection)); }
+      if (null == actualType) { throw new ArgumentNullException(nameof(actualType)); }
+
+      var streamAttr = SerializationManager.GetStreamProvider(actualType, expectedType);
+      var eventDatas = SerializationManager.SerializeEvents(streamAttr, actualType, events, eventContexts, expectedType);
+      if (streamAttr != null)
+      {
+        return await DoWriteAsync(connection, streamAttr.StreamId, streamAttr.ExpectedVersion, eventDatas, batchSize, userCredentials).ConfigureAwait(false);
+      }
+      else
+      {
+        return await DoWriteAsync(connection, JsonConvertX.SerializeTypeName(expectedType ?? actualType), ExpectedVersion.Any, eventDatas, batchSize, userCredentials).ConfigureAwait(false);
+      }
+    }
+
+
+
+
+    public static async Task<WriteResult> PublishEventsAsync<TEvent>(this IEventStoreConnectionBase connection, string topic, int batchSize, ICollection<TEvent> events,
+      Dictionary<string, object> eventContext = null, Type expectedType = null, UserCredentials userCredentials = null)
+      where TEvent : class
+    {
+      var actualType = typeof(TEvent);
+      if (actualType == TypeHelper.ObjectType)
+      {
+        if (null == connection) { throw new ArgumentNullException(nameof(connection)); }
+        if (string.IsNullOrWhiteSpace(topic)) { throw new ArgumentNullException(nameof(topic)); }
+        if (null == events) { throw new ArgumentNullException(nameof(events)); }
+
+        // 无法确定所发布事件是否在同一 stream 中，不能采用事物
+        WriteResult result = default(WriteResult);
+        foreach (var @event in events)
+        {
+          result = await PublishEventAsync(connection, topic, @event.GetType(), @event, eventContext, expectedType, userCredentials).ConfigureAwait(false);
+        }
+        return result;
+      }
+
+      return await PublishEventsAsync(connection, topic, actualType, batchSize, events, eventContext, expectedType, userCredentials).ConfigureAwait(false);
+    }
+    public static async Task<WriteResult> PublishEventsAsync<TEvent>(this IEventStoreConnectionBase connection, string topic, Type actualType, int batchSize, ICollection<TEvent> events,
+      Dictionary<string, object> eventContext = null, Type expectedType = null, UserCredentials userCredentials = null)
+      where TEvent : class
+    {
+      if (batchSize <= 0) { throw new ArgumentOutOfRangeException(nameof(batchSize)); }
+      if (null == events) { throw new ArgumentNullException(nameof(events)); }
+
+      if (events.Count <= batchSize)
+      {
+        return await PublishEventsAsync(connection, topic, actualType, events, eventContext, expectedType, userCredentials).ConfigureAwait(false);
+      }
+
+      if (null == connection) { throw new ArgumentNullException(nameof(connection)); }
+      if (string.IsNullOrWhiteSpace(topic)) { throw new ArgumentNullException(nameof(topic)); }
+      if (null == actualType) { throw new ArgumentNullException(nameof(actualType)); }
+
+      var streamAttr = SerializationManager.GetStreamProvider(actualType, expectedType);
+      var eventDatas = SerializationManager.SerializeEvents(streamAttr, actualType, events, eventContext, expectedType);
+      if (streamAttr != null)
+      {
+        return await DoWriteAsync(connection, CombineStreamId(streamAttr.StreamId, topic), streamAttr.ExpectedVersion, eventDatas, batchSize, userCredentials).ConfigureAwait(false);
+      }
+      else
+      {
+        return await DoWriteAsync(connection, CombineStreamId(JsonConvertX.SerializeTypeName(expectedType ?? actualType), topic), ExpectedVersion.Any, eventDatas, batchSize, userCredentials).ConfigureAwait(false);
+      }
+    }
+
+
+
+
+    public static async Task<WriteResult> PublishEventsAsync<TEvent>(this IEventStoreConnectionBase connection, string topic, int batchSize, IList<TEvent> events,
+      IList<Dictionary<string, object>> eventContexts, Type expectedType = null, UserCredentials userCredentials = null)
+      where TEvent : class
+    {
+      var actualType = typeof(TEvent);
+      if (actualType == TypeHelper.ObjectType)
+      {
+        if (null == connection) { throw new ArgumentNullException(nameof(connection)); }
+        if (string.IsNullOrWhiteSpace(topic)) { throw new ArgumentNullException(nameof(topic)); }
+        if (null == events) { throw new ArgumentNullException(nameof(events)); }
+        if (null == eventContexts) { throw new ArgumentNullException(nameof(eventContexts)); }
+        if (events.Count != eventContexts.Count) { throw new ArgumentOutOfRangeException(nameof(eventContexts)); }
+
+        // 无法确定所发布事件是否在同一 stream 中，不能采用事物
+        WriteResult result = default(WriteResult);
+        for (var idx = 0; idx < events.Count; idx++)
+        {
+          var @event = events[idx];
+          result = await PublishEventAsync(connection, topic, @event.GetType(), @event, eventContexts[idx], expectedType, userCredentials).ConfigureAwait(false);
+        }
+        return result;
+      }
+      return await PublishEventsAsync(connection, topic, actualType, batchSize, events, eventContexts, expectedType, userCredentials).ConfigureAwait(false);
+    }
+    public static async Task<WriteResult> PublishEventsAsync<TEvent>(this IEventStoreConnectionBase connection, string topic, Type actualType, int batchSize, IList<TEvent> events,
+      IList<Dictionary<string, object>> eventContexts, Type expectedType = null, UserCredentials userCredentials = null)
+      where TEvent : class
+    {
+      if (batchSize <= 0) { throw new ArgumentOutOfRangeException(nameof(batchSize)); }
+      if (null == events) { throw new ArgumentNullException(nameof(events)); }
+
+      if (events.Count <= batchSize)
+      {
+        return await PublishEventsAsync(connection, topic, actualType, events, eventContexts, expectedType, userCredentials).ConfigureAwait(false);
+      }
+
+      if (null == connection) { throw new ArgumentNullException(nameof(connection)); }
+      if (string.IsNullOrWhiteSpace(topic)) { throw new ArgumentNullException(nameof(topic)); }
+      if (null == actualType) { throw new ArgumentNullException(nameof(actualType)); }
+
+      var streamAttr = SerializationManager.GetStreamProvider(actualType, expectedType);
+      var eventDatas = SerializationManager.SerializeEvents(streamAttr, actualType, events, eventContexts, expectedType);
+      if (streamAttr != null)
+      {
+        return await DoWriteAsync(connection, CombineStreamId(streamAttr.StreamId, topic), streamAttr.ExpectedVersion, eventDatas, batchSize, userCredentials).ConfigureAwait(false);
+      }
+      else
+      {
+        return await DoWriteAsync(connection, CombineStreamId(JsonConvertX.SerializeTypeName(expectedType ?? actualType), topic), ExpectedVersion.Any, eventDatas, batchSize, userCredentials).ConfigureAwait(false);
+      }
     }
 
     #endregion
--- src/EventStore.ClientAPI/IEventStoreConnectionExtensions.Send.cs
+++ src/EventStore.ClientAPI/IEventStoreConnectionExtensions.Send.cs
@@ -48,12 +48,11 @@ namespace EventStore.ClientAPI
       Dictionary<string, object> eventContext = null, Type expectedType = null, UserCredentials userCredentials = null)
       where TEvent : class
     {
-      if (null == connection) { throw new ArgumentNullException(nameof(connection)); }
-      if (string.IsNullOrWhiteSpace(stream)) { throw new ArgumentNullException(nameof(stream)); }
-
       var actualType = typeof(TEvent);
       if (actualType == TypeHelper.ObjectType)
       {
+        if (null == connection) { throw new ArgumentNullException(nameof(connection)); }
+        if (string.IsNullOrWhiteSpace(stream)) { throw new ArgumentNullException(nameof(stream)); }
         if (null == events) { throw new ArgumentNullException(nameof(events)); }
         //var eventDatas = SerializationManager.SerializeEvents(events, eventContext, expectedType);
         var eventDatas = events.Select(_ => SerializationManager.SerializeEvent(_, eventContext, expectedType)).ToArray();
@@ -76,44 +75,6 @@ namespace EventStore.ClientAPI
 
       return connection.AppendToStreamAsync(stream, streamAttr != null ? streamAttr.ExpectedVersion : ExpectedVersion.Any, eventDatas, userCredentials);
     }
-    public static Task<WriteResult> SendEventsAsync<TEvent>(this IEventStoreConnectionBase connection, string stream, int batchSize,
-      ICollection<TEvent> events, Dictionary<string, object> eventContext = null, Type expectedType = null, UserCredentials userCredentials = null)
-      where TEvent : class
-    {
-      if (null == connection) { throw new ArgumentNullException(nameof(connection)); }
-      if (string.IsNullOrWhiteSpace(stream)) { throw new ArgumentNullException(nameof(stream)); }
-
-      var actualType = typeof(TEvent);
-      if (actualType == TypeHelper.ObjectType)
-      {
-        if (null == events) { throw new ArgumentNullException(nameof(events)); }
-        //var eventDatas = SerializationManager.SerializeEvents(events, eventContext, expectedType);
-        var eventDatas = events.Select(_ => SerializationManager.SerializeEvent(_, eventContext, expectedType)).ToArray();
-        return connection.AppendToStreamAsync(stream, ExpectedVersion.Any, eventDatas, userCredentials);
-      }
-      return SendEventsAsync(connection, stream, actualType, events, eventContext, expectedType, userCredentials);
-    }
-    public static Task<WriteResult> SendEventsAsync<TEvent>(this IEventStoreConnectionBase connection, string stream, int batchSize, Type actualType,
-      ICollection<TEvent> events, Dictionary<string, object> eventContext = null, Type expectedType = null, UserCredentials userCredentials = null)
-      where TEvent : class
-    {
-      if (batchSize <= 0) { throw new ArgumentOutOfRangeException(nameof(batchSize)); }
-      if (null == events) { throw new ArgumentNullException(nameof(events)); }
-      if (events.Count <= batchSize)
-      {
-        return SendEventsAsync(connection, stream, actualType, events: events, eventContext: eventContext, expectedType: expectedType, userCredentials: userCredentials);
-      }
-
-      if (null == connection) { throw new ArgumentNullException(nameof(connection)); }
-      if (string.IsNullOrWhiteSpace(stream)) { throw new ArgumentNullException(nameof(stream)); }
-      if (null == actualType) { throw new ArgumentNullException(nameof(actualType)); }
-
-      var streamAttr = SerializationManager.GetStreamProvider(actualType, expectedType);
-
-      var eventDatas = SerializationManager.SerializeEvents(streamAttr, actualType, events, eventContext, expectedType);
-
-      return connection.AppendToStreamAsync(stream, streamAttr != null ? streamAttr.ExpectedVersion : ExpectedVersion.Any, eventDatas, userCredentials);
-    }
 
 
 
@@ -122,13 +83,13 @@ namespace EventStore.ClientAPI
       IList<Dictionary<string, object>> eventContexts, Type expectedType = null, UserCredentials userCredentials = null)
       where TEvent : class
     {
-      if (null == connection) { throw new ArgumentNullException(nameof(connection)); }
-      if (string.IsNullOrWhiteSpace(stream)) { throw new ArgumentNullException(nameof(stream)); }
-      //if (null == events) { throw new ArgumentNullException(nameof(events)); }
-
       var actualType = typeof(TEvent);
       if (actualType == TypeHelper.ObjectType)
       {
+        if (null == connection) { throw new ArgumentNullException(nameof(connection)); }
+        if (string.IsNullOrWhiteSpace(stream)) { throw new ArgumentNullException(nameof(stream)); }
+        //if (null == events) { throw new ArgumentNullException(nameof(events)); }
+
         var eventDatas = SerializationManager.SerializeEvents(events, eventContexts, expectedType);
         return connection.AppendToStreamAsync(stream, ExpectedVersion.Any, eventDatas, userCredentials);
       }
@@ -148,6 +109,126 @@ namespace EventStore.ClientAPI
       var eventDatas = SerializationManager.SerializeEvents(streamAttr, actualType, events, eventContexts, expectedType);
 
       return connection.AppendToStreamAsync(stream, streamAttr != null ? streamAttr.ExpectedVersion : ExpectedVersion.Any, eventDatas, userCredentials);
+    }
+
+    #endregion
+
+    #region -- SendEventsAsync Using Transaction --
+
+    public static async Task<WriteResult> SendEventsAsync<TEvent>(this IEventStoreConnectionBase connection, string stream, int batchSize,
+      ICollection<TEvent> events, Dictionary<string, object> eventContext = null, Type expectedType = null, UserCredentials userCredentials = null)
+      where TEvent : class
+    {
+      var actualType = typeof(TEvent);
+      if (actualType == TypeHelper.ObjectType)
+      {
+        if (null == connection) { throw new ArgumentNullException(nameof(connection)); }
+        if (string.IsNullOrWhiteSpace(stream)) { throw new ArgumentNullException(nameof(stream)); }
+        if (batchSize <= 0) { throw new ArgumentOutOfRangeException(nameof(batchSize)); }
+        if (null == events) { throw new ArgumentNullException(nameof(events)); }
+
+        //var eventDatas = SerializationManager.SerializeEvents(events, eventContext, expectedType);
+        var eventDatas = events.Select(_ => SerializationManager.SerializeEvent(_, eventContext, expectedType)).ToArray();
+        if (events.Count <= batchSize)
+        {
+          return await connection.AppendToStreamAsync(stream, ExpectedVersion.Any, eventDatas, userCredentials).ConfigureAwait(false);
+        }
+        else
+        {
+          return await DoWriteAsync(connection, stream, ExpectedVersion.Any, eventDatas, batchSize, userCredentials).ConfigureAwait(false);
+        }
+      }
+      return await SendEventsAsync(connection, stream, actualType, batchSize, events, eventContext, expectedType, userCredentials).ConfigureAwait(false);
+    }
+    public static async Task<WriteResult> SendEventsAsync<TEvent>(this IEventStoreConnectionBase connection, string stream, Type actualType, int batchSize,
+      ICollection<TEvent> events, Dictionary<string, object> eventContext = null, Type expectedType = null, UserCredentials userCredentials = null)
+      where TEvent : class
+    {
+      if (batchSize <= 0) { throw new ArgumentOutOfRangeException(nameof(batchSize)); }
+      if (null == events) { throw new ArgumentNullException(nameof(events)); }
+
+      if (events.Count <= batchSize)
+      {
+        return await SendEventsAsync(connection, stream, actualType, events, eventContext, expectedType, userCredentials).ConfigureAwait(false);
+      }
+
+      if (null == connection) { throw new ArgumentNullException(nameof(connection)); }
+      if (string.IsNullOrWhiteSpace(stream)) { throw new ArgumentNullException(nameof(stream)); }
+      if (null == actualType) { throw new ArgumentNullException(nameof(actualType)); }
+
+      var streamAttr = SerializationManager.GetStreamProvider(actualType, expectedType);
+      var expectedVersion = streamAttr != null ? streamAttr.ExpectedVersion : ExpectedVersion.Any;
+      var eventDatas = SerializationManager.SerializeEvents(streamAttr, actualType, events, eventContext, expectedType);
+      return await DoWriteAsync(connection, stream, expectedVersion, eventDatas, batchSize, userCredentials).ConfigureAwait(false);
+    }
+
+
+
+
+    public static async Task<WriteResult> SendEventsAsync<TEvent>(this IEventStoreConnectionBase connection, string stream, int batchSize,
+      IList<TEvent> events, IList<Dictionary<string, object>> eventContexts, Type expectedType = null, UserCredentials userCredentials = null)
+      where TEvent : class
+    {
+      var actualType = typeof(TEvent);
+      if (actualType == TypeHelper.ObjectType)
+      {
+        if (null == connection) { throw new ArgumentNullException(nameof(connection)); }
+        if (string.IsNullOrWhiteSpace(stream)) { throw new ArgumentNullException(nameof(stream)); }
+        if (batchSize <= 0) { throw new ArgumentOutOfRangeException(nameof(batchSize)); }
+        if (null == events) { throw new ArgumentNullException(nameof(events)); }
+
+        var eventDatas = SerializationManager.SerializeEvents(events, eventContexts, expectedType);
+        if (events.Count <= batchSize)
+        {
+          return await connection.AppendToStreamAsync(stream, ExpectedVersion.Any, eventDatas, userCredentials).ConfigureAwait(false);
+        }
+        else
+        {
+          return await DoWriteAsync(connection, stream, ExpectedVersion.Any, eventDatas, batchSize, userCredentials).ConfigureAwait(false);
+        }
+      }
+      return await SendEventsAsync(connection, stream, actualType, batchSize, events, eventContexts, expectedType, userCredentials).ConfigureAwait(false);
+    }
+    public static async Task<WriteResult> SendEventsAsync<TEvent>(this IEventStoreConnectionBase connection, string stream, Type actualType, int batchSize,
+      IList<TEvent> events, IList<Dictionary<string, object>> eventContexts, Type expectedType = null, UserCredentials userCredentials = null)
+      where TEvent : class
+    {
+      if (batchSize <= 0) { throw new ArgumentOutOfRangeException(nameof(batchSize)); }
+      if (null == events) { throw new ArgumentNullException(nameof(events)); }
+
+      if (events.Count <= batchSize)
+      {
+        return await SendEventsAsync(connection, stream, actualType, events, eventContexts, expectedType, userCredentials).ConfigureAwait(false);
+      }
+
+      if (null == connection) { throw new ArgumentNullException(nameof(connection)); }
+      if (string.IsNullOrWhiteSpace(stream)) { throw new ArgumentNullException(nameof(stream)); }
+      if (null == actualType) { throw new ArgumentNullException(nameof(actualType)); }
+
+      var streamAttr = SerializationManager.GetStreamProvider(actualType, expectedType);
+      var expectedVersion = streamAttr != null ? streamAttr.ExpectedVersion : ExpectedVersion.Any;
+      var eventDatas = SerializationManager.SerializeEvents(streamAttr, actualType, events, eventContexts, expectedType);
+      return await DoWriteAsync(connection, stream, expectedVersion, eventDatas, batchSize, userCredentials).ConfigureAwait(false);
+    }
+
+    #endregion
+
+    #region ** DoWriteAsync **
+
+    private static async Task<WriteResult> DoWriteAsync(IEventStoreConnectionBase connection, string stream, long expectedVersion,
+      EventData[] eventDatas, int batchSize, UserCredentials userCredentials)
+    {
+      using (var trans = await connection.StartTransactionAsync(stream, expectedVersion, userCredentials))
+      {
+        var page = 0;
+        while (page < eventDatas.Length)
+        {
+          await trans.WriteAsync(eventDatas.Skip(page).Take(batchSize)).ConfigureAwait(false);
+          page += batchSize;
+        }
+
+        return await trans.CommitAsync().ConfigureAwait(false);
+      }
     }
 
     #endregion
