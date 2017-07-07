StetHD/EventStore

Limit pending send bytes to a configurable amount and drop tcp messages if the connection has been closed.

src/EventStore.ClusterNode/ClusterNodeOptions.cs
@@ -73,6 +73,8 @@ namespace EventStore.ClusterNode
         public int ExtTcpHeartbeatInterval { get; set; }
         [ArgDescription(Opts.GossipOnSingleNodeDescr, Opts.InterfacesGroup)]
         public bool GossipOnSingleNode { get; set; }
+        [ArgDescription(Opts.ConnectionPendingSendBytesThresholdDescr, Opts.AppGroup)]
+        public int ConnectionPendingSendBytesThreshold {get;set;}
 
 
         [ArgDescription(Opts.ForceDescr, Opts.AppGroup)]

cuteant/EventStore-DotNetty-Fork

src/EventStore.ClusterNode/ClusterNodeOptions.cs
@@ -73,6 +73,8 @@ namespace EventStore.ClusterNode
     public int ExtTcpHeartbeatInterval { get; set; }
     [ArgDescription(Opts.GossipOnSingleNodeDescr, Opts.InterfacesGroup)]
     public bool GossipOnSingleNode { get; set; }
+    [ArgDescription(Opts.ConnectionPendingSendBytesThresholdDescr, Opts.InterfacesGroup)]
+    public int ConnectionPendingSendBytesThreshold {get;set;}
 
 
     [ArgDescription(Opts.ForceDescr, Opts.AppGroup)]