b19d1e6dd3fc54e473293a05ca7fe9ef092b81b2
Pieter Germishuys <pieter@pieterg.com>
2017-07-14 08:12:58 +0100

    Revert "Rework the the emitted stream to batch up writes"

--- src/EventStore.Projections.Core/Services/Processing/ProjectionCheckpoint.cs
+++ src/EventStore.Projections.Core/Services/Processing/ProjectionCheckpoint.cs
@@ -1,253 +1,211 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Principal;
-using Microsoft.Extensions.Logging;
+using EventStore.Common.Log;
 using EventStore.Core.Helpers;
 using EventStore.Core.Messaging;
 using EventStore.Projections.Core.Messages;
-#if DESKTOPCLR
-using Nessos.LinqOptimizer.CSharp;
-#endif
 
 namespace EventStore.Projections.Core.Services.Processing
 {
-  public class ProjectionCheckpoint : IDisposable, IEmittedStreamContainer, IEventWriter
-  {
-    private readonly int _maxWriteBatchLength;
-    private readonly ILogger _logger;
-
-    private readonly Dictionary<string, EmittedStream> _emittedStreams = new Dictionary<string, EmittedStream>(StringComparer.Ordinal);
-    private readonly IPrincipal _runAs;
-    private readonly CheckpointTag _from;
-    private CheckpointTag _last;
-    private readonly IProjectionCheckpointManager _readyHandler;
-    private readonly PositionTagger _positionTagger;
-
-    private bool _checkpointRequested = false;
-    private int _requestedCheckpoints;
-    private bool _started = false;
-    private bool _processing = false;
-
-    private readonly IODispatcher _ioDispatcher;
-
-    private readonly ProjectionVersion _projectionVersion;
-
-    private Queue<EmittedStream> _queue;
-
-    public ProjectionCheckpoint(
-        IODispatcher ioDispatcher,
-        ProjectionVersion projectionVersion,
-        IPrincipal runAs,
-        IProjectionCheckpointManager readyHandler,
-        CheckpointTag from,
-        PositionTagger positionTagger,
-        int maxWriteBatchLength,
-        ILogger logger = null)
+    public class ProjectionCheckpoint : IDisposable, IEmittedStreamContainer, IEventWriter
     {
-      if (ioDispatcher == null) throw new ArgumentNullException("ioDispatcher");
-      if (readyHandler == null) throw new ArgumentNullException("readyHandler");
-      if (positionTagger == null) throw new ArgumentNullException("positionTagger");
-      if (from.CommitPosition < from.PreparePosition) throw new ArgumentException("from");
-      //NOTE: fromCommit can be equal fromPrepare on 0 position.  Is it possible anytime later? Ignoring for now.
-      _ioDispatcher = ioDispatcher;
-      _projectionVersion = projectionVersion;
-      _runAs = runAs;
-      _readyHandler = readyHandler;
-      _positionTagger = positionTagger;
-      _from = _last = from;
-      _maxWriteBatchLength = maxWriteBatchLength;
-      _logger = logger;
-      _queue = new Queue<EmittedStream>();
-    }
+        private readonly int _maxWriteBatchLength;
+        private readonly ILogger _logger;
 
-    public void Start()
-    {
-      if (_started)
-        throw new InvalidOperationException("Projection has been already started");
-      _started = true;
-      foreach (var stream in _emittedStreams.Values)
-      {
-        _queue.Enqueue(stream);
-      }
-      ProcessQueue();
-    }
+        private readonly Dictionary<string, EmittedStream> _emittedStreams = new Dictionary<string, EmittedStream>();
+        private readonly IPrincipal _runAs;
+        private readonly CheckpointTag _from;
+        private CheckpointTag _last;
+        private readonly IProjectionCheckpointManager _readyHandler;
+        private readonly PositionTagger _positionTagger;
 
-    public void ValidateOrderAndEmitEvents(EmittedEventEnvelope[] events)
-    {
-      UpdateLastPosition(events);
-      EnsureCheckpointNotRequested();
+        private bool _checkpointRequested = false;
+        private int _requestedCheckpoints;
+        private bool _started = false;
 
-      var groupedEvents = events.GroupBy(v => v.Event.StreamId);
-      foreach (var eventGroup in groupedEvents)
-      {
-        EmitEventsToStream(eventGroup.Key, eventGroup.ToArray());
-      }
-      ProcessQueue();
-    }
+        private readonly IODispatcher _ioDispatcher;
 
-    private void UpdateLastPosition(EmittedEventEnvelope[] events)
-    {
-      foreach (var emittedEvent in events)
-      {
-        if (emittedEvent.Event.CausedByTag > _last)
-          _last = emittedEvent.Event.CausedByTag;
-      }
-    }
+        private readonly ProjectionVersion _projectionVersion;
 
-    private void ValidateCheckpointPosition(CheckpointTag position)
-    {
-      if (position <= _from)
-        throw new InvalidOperationException(
-            string.Format(
-                "Checkpoint position before or equal to the checkpoint start position. Requested: '{0}' Started: '{1}'",
-                position, _from));
-      if (position < _last)
-        throw new InvalidOperationException(
-            string.Format(
-                "Checkpoint position before last handled position. Requested: '{0}' Last: '{1}'", position,
-                _last));
-    }
+        private List<IEnvelope> _awaitingStreams;
 
-    public void Prepare(CheckpointTag position)
-    {
-      if (!_started)
-        throw new InvalidOperationException("Projection has not been started");
-      ValidateCheckpointPosition(position);
-      _checkpointRequested = true;
-      _requestedCheckpoints = 1; // avoid multiple checkpoint ready messages if already ready
-      foreach (var emittedStream in _emittedStreams.Values)
-      {
-        _requestedCheckpoints++;
-        emittedStream.Checkpoint();
-      }
-      _requestedCheckpoints--;
-      OnCheckpointCompleted();
-    }
-
-    private void EnsureCheckpointNotRequested()
-    {
-      if (_checkpointRequested)
-        throw new InvalidOperationException("Checkpoint requested");
-    }
-
-    private void EmitEventsToStream(string streamId, EmittedEventEnvelope[] emittedEvents)
-    {
-      if (string.IsNullOrEmpty(streamId))
-        throw new ArgumentNullException("streamId");
-      EmittedStream stream;
-      if (!_emittedStreams.TryGetValue(streamId, out stream))
-      {
-        var streamMetadata = emittedEvents.Length > 0 ? emittedEvents[0].StreamMetadata : null;
-
-        var writerConfiguration = new EmittedStream.WriterConfiguration(
-            streamMetadata, _runAs, maxWriteBatchLength: _maxWriteBatchLength, logger: _logger);
-
-        stream = new EmittedStream(
-            streamId, writerConfiguration, _projectionVersion, _positionTagger, _from, _ioDispatcher, this);
-
-        _emittedStreams.Add(streamId, stream);
-        _queue.Enqueue(stream);
-      }
-      stream.EmitEvents(emittedEvents.Select(v => v.Event).ToArray());
-    }
-
-    public void Handle(CoreProjectionProcessingMessage.ReadyForCheckpoint message)
-    {
-      _requestedCheckpoints--;
-      OnCheckpointCompleted();
-    }
-
-    private void OnCheckpointCompleted()
-    {
-      if (_requestedCheckpoints == 0)
-      {
-        _readyHandler.Handle(new CoreProjectionProcessingMessage.ReadyForCheckpoint(this));
-      }
-    }
-
-    public int GetWritePendingEvents()
-    {
-#if DESKTOPCLR
-      return _emittedStreams.Values.AsQueryExpr().Select(v => v.GetWritePendingEvents()).Sum().Run();
-#else
-      return _emittedStreams.Values.Sum(v => v.GetWritePendingEvents());
-#endif
-    }
-
-    public int GetWritesInProgress()
-    {
-#if DESKTOPCLR
-      return _emittedStreams.Values.AsQueryExpr().Select(v => v.GetWritesInProgress()).Sum().Run();
-#else
-      return _emittedStreams.Values.Sum(v => v.GetWritesInProgress());
-#endif
-    }
-
-    public int GetReadsInProgress()
-    {
-#if DESKTOPCLR
-      return _emittedStreams.Values.AsQueryExpr().Select(v => v.GetReadsInProgress()).Sum().Run();
-#else
-      return _emittedStreams.Values.Sum(v => v.GetReadsInProgress());
-#endif
-    }
-
-    public void Handle(CoreProjectionProcessingMessage.RestartRequested message)
-    {
-      _readyHandler.Handle(message);
-    }
-
-    public void Handle(CoreProjectionProcessingMessage.Failed message)
-    {
-      _readyHandler.Handle(message);
-    }
-
-    public void Dispose()
-    {
-      if (_emittedStreams != null)
-        foreach (var stream in _emittedStreams.Values)
-          stream.Dispose();
-    }
-
-    public void Handle(CoreProjectionProcessingMessage.EmittedStreamAwaiting message)
-    {
-      _processing = false;
-      ProcessQueue();
-    }
-
-    public void Handle(CoreProjectionProcessingMessage.EmittedStreamWriteCompleted message)
-    {
-      _processing = false;
-      ProcessQueue();
-    }
-
-    private void ProcessQueue()
-    {
-      if (!_started || _processing) return;
-
-      _processing = true;
-      for (int i = 0; i < _queue.Count; i++)
-      {
-        EmittedStream emittedStream = null;
-        try
+        public ProjectionCheckpoint(
+            IODispatcher ioDispatcher,
+            ProjectionVersion projectionVersion,
+            IPrincipal runAs,
+            IProjectionCheckpointManager readyHandler,
+            CheckpointTag from,
+            PositionTagger positionTagger,
+            int maxWriteBatchLength,
+            ILogger logger = null)
         {
-          emittedStream = _queue.Dequeue();
-          if (emittedStream != null)
-          {
-            if (emittedStream.GetWritePendingEvents() > 0)
+            if (ioDispatcher == null) throw new ArgumentNullException("ioDispatcher");
+            if (readyHandler == null) throw new ArgumentNullException("readyHandler");
+            if (positionTagger == null) throw new ArgumentNullException("positionTagger");
+            if (from.CommitPosition < from.PreparePosition) throw new ArgumentException("from");
+            //NOTE: fromCommit can be equal fromPrepare on 0 position.  Is it possible anytime later? Ignoring for now.
+            _ioDispatcher = ioDispatcher;
+            _projectionVersion = projectionVersion;
+            _runAs = runAs;
+            _readyHandler = readyHandler;
+            _positionTagger = positionTagger;
+            _from = _last = from;
+            _maxWriteBatchLength = maxWriteBatchLength;
+            _logger = logger;
+        }
+
+        public void Start()
+        {
+            if (_started)
+                throw new InvalidOperationException("Projection has been already started");
+            _started = true;
+            foreach (var stream in _emittedStreams.Values)
             {
-              emittedStream.ProcessQueue();
-              return;
+                stream.Start();
             }
-          }
         }
-        finally
+
+        public void ValidateOrderAndEmitEvents(EmittedEventEnvelope[] events)
         {
-          if(emittedStream != null) { _queue.Enqueue(emittedStream); }
+            UpdateLastPosition(events);
+            EnsureCheckpointNotRequested();
+
+            var groupedEvents = events.GroupBy(v => v.Event.StreamId);
+            foreach (var eventGroup in groupedEvents)
+            {
+                EmitEventsToStream(eventGroup.Key, eventGroup.ToArray());
+            }
         }
-      }
-      _processing = false;
+
+        private void UpdateLastPosition(EmittedEventEnvelope[] events)
+        {
+            foreach (var emittedEvent in events)
+            {
+                if (emittedEvent.Event.CausedByTag > _last)
+                    _last = emittedEvent.Event.CausedByTag;
+            }
+        }
+
+        private void ValidateCheckpointPosition(CheckpointTag position)
+        {
+            if (position <= _from)
+                throw new InvalidOperationException(
+                    string.Format(
+                        "Checkpoint position before or equal to the checkpoint start position. Requested: '{0}' Started: '{1}'",
+                        position, _from));
+            if (position < _last)
+                throw new InvalidOperationException(
+                    string.Format(
+                        "Checkpoint position before last handled position. Requested: '{0}' Last: '{1}'", position,
+                        _last));
+        }
+
+        public void Prepare(CheckpointTag position)
+        {
+            if (!_started)
+                throw new InvalidOperationException("Projection has not been started");
+            ValidateCheckpointPosition(position);
+            _checkpointRequested = true;
+            _requestedCheckpoints = 1; // avoid multiple checkpoint ready messages if already ready
+            foreach (var emittedStream in _emittedStreams.Values)
+            {
+                _requestedCheckpoints++;
+                emittedStream.Checkpoint();
+            }
+            _requestedCheckpoints--;
+            OnCheckpointCompleted();
+        }
+
+        private void EnsureCheckpointNotRequested()
+        {
+            if (_checkpointRequested)
+                throw new InvalidOperationException("Checkpoint requested");
+        }
+
+        private void EmitEventsToStream(string streamId, EmittedEventEnvelope[] emittedEvents)
+        {
+            if (string.IsNullOrEmpty(streamId))
+                throw new ArgumentNullException("streamId");
+            EmittedStream stream;
+            if (!_emittedStreams.TryGetValue(streamId, out stream))
+            {
+                var streamMetadata = emittedEvents.Length > 0 ? emittedEvents[0].StreamMetadata : null;
+
+                var writerConfiguration = new EmittedStream.WriterConfiguration(
+                    streamMetadata, _runAs, maxWriteBatchLength: _maxWriteBatchLength, logger: _logger);
+
+                stream = new EmittedStream(
+                    streamId, writerConfiguration, _projectionVersion, _positionTagger, _from, _ioDispatcher, this);
+
+                if (_started)
+                    stream.Start();
+                _emittedStreams.Add(streamId, stream);
+            }
+            stream.EmitEvents(emittedEvents.Select(v => v.Event).ToArray());
+        }
+
+        public void Handle(CoreProjectionProcessingMessage.ReadyForCheckpoint message)
+        {
+            _requestedCheckpoints--;
+            OnCheckpointCompleted();
+        }
+
+        private void OnCheckpointCompleted()
+        {
+            if (_requestedCheckpoints == 0)
+            {
+                _readyHandler.Handle(new CoreProjectionProcessingMessage.ReadyForCheckpoint(this));
+            }
+        }
+
+        public int GetWritePendingEvents()
+        {
+            return _emittedStreams.Values.Sum(v => v.GetWritePendingEvents());
+        }
+
+        public int GetWritesInProgress()
+        {
+            return _emittedStreams.Values.Sum(v => v.GetWritesInProgress());
+        }
+
+        public int GetReadsInProgress()
+        {
+            return _emittedStreams.Values.Sum(v => v.GetReadsInProgress());
+        }
+
+        public void Handle(CoreProjectionProcessingMessage.RestartRequested message)
+        {
+            _readyHandler.Handle(message);
+        }
+
+        public void Handle(CoreProjectionProcessingMessage.Failed message)
+        {
+            _readyHandler.Handle(message);
+        }
+
+        public void Dispose()
+        {
+            if (_emittedStreams != null)
+                foreach (var stream in _emittedStreams.Values)
+                    stream.Dispose();
+        }
+
+        public void Handle(CoreProjectionProcessingMessage.EmittedStreamAwaiting message)
+        {
+            if (_awaitingStreams == null)
+                _awaitingStreams = new List<IEnvelope>();
+            _awaitingStreams.Add(message.Envelope);
+        }
+
+        public void Handle(CoreProjectionProcessingMessage.EmittedStreamWriteCompleted message)
+        {
+            var awaitingStreams = _awaitingStreams;
+            _awaitingStreams = null; // still awaiting will re-register
+            if (awaitingStreams != null)
+                foreach (var stream in awaitingStreams)
+                    stream.ReplyWith(message);
+        }
     }
-  }
 }
