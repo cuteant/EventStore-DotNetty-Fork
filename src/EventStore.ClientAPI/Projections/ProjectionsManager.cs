cd4aa74dde6339540bc3783ebcba4216d464eda6
Abdelmawla Mohamed <abdo.talema@gainclearview.com.com>
2017-10-21 06:48:07 +1100

    fixed whiile updating system projections query, wasn't picking right type

--- src/EventStore.ClientAPI/Projections/ProjectionsManager.cs
+++ src/EventStore.ClientAPI/Projections/ProjectionsManager.cs
@@ -285,6 +285,21 @@ namespace EventStore.ClientAPI.Projections
     }
 
     /// <summary>
+    /// Asynchronously updates the definition of a query.
+    /// </summary>
+    /// <param name="name">The name of the query.</param>
+    /// <param name="query">The JavaScript source code of the query.</param>
+    /// <param name="userCredentials">Credentials for the operation.</param>
+    /// <param name="emitEnabled">Allow projection to send events to other streams</param>
+    public Task UpdateQueryAsync(string name, string query, bool? emitEnabled = null, UserCredentials userCredentials = null)
+    {
+      Ensure.NotNullOrEmpty(name, nameof(name));
+      Ensure.NotNullOrEmpty(query, nameof(query));
+
+      return _client.UpdateQuery(_httpEndPoint, name, query, userCredentials, _httpSchema, emitEnabled);
+    }
+
+    /// <summary>
     /// Asynchronously deletes a projection 
     /// </summary>
     /// <param name="name">The name of the projection.</param>
