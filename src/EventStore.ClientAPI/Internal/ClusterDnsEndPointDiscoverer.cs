6381ac5af8c8c8dcce81d4ffd8ef1322c6f8ef57
Greg Young <gregoryyoung1@gmail.com>
2018-04-02 14:56:05 +0700

    add ability to add custom node discovery

--- src/EventStore.ClientAPI/Internal/ClusterDnsEndPointDiscoverer.cs
+++ src/EventStore.ClientAPI/Internal/ClusterDnsEndPointDiscoverer.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
+using EventStore.ClientAPI;
 using EventStore.ClientAPI.Common.Utils;
 using EventStore.ClientAPI.Exceptions;
 using EventStore.ClientAPI.Messages;
