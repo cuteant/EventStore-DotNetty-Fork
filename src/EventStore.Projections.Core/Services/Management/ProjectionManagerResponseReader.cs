joegana/EventStore

Add timeout functionality to IODispatcher.
This allows us to handle timeouts on reads for projections in a single place.
Update the projection command/response readers, checkpoint managers and emitted streams tracker/deleter to handle timeouts.
This is to prevent projections from getting stuck in the event of a timeout.

src/EventStore.Projections.Core/Services/Management/ProjectionManagerResponseReader.cs
@@ -118,6 +118,10 @@ namespace EventStore.Projections.Core.Services.Management
                     false,
                     SystemAccount.Principal,
                     ReadForwardCompleted,
+                    () => {
+                        Log.Debug("Read forward of stream {0} timed out. Retrying", ProjectionNamesBuilder._projectionsMasterStream);
+                        ReadForward();
+                    },
                     _correlationId)
             );
             _publisher.Publish(TimerMessage.Schedule.Create(

cuteant/EventStore-DotNetty-Fork

src/EventStore.Projections.Core/Services/Management/ProjectionManagerResponseReader.cs
@@ -119,6 +119,10 @@ namespace EventStore.Projections.Core.Services.Management
                     false,
                     SystemAccount.Principal,
                     ReadForwardCompleted,
+                    () => {
+                        Log.Warn("Read forward of stream {0} timed out. Retrying", ProjectionNamesBuilder._projectionsMasterStream);
+                        ReadForward();
+                    },
                     _correlationId)
             );
             _publisher.Publish(TimerMessage.Schedule.Create(