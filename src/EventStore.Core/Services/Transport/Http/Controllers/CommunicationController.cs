394cac44b37ed5a66e5e7324bbd4536084049317
shaan1337 <sniper111@gmail.com>
2018-05-30 16:01:27 +0400

    Fix bug in MakeUrl() where url is wrongly formed when query parameters are present Add failing test

--- src/EventStore.Core/Services/Transport/Http/Controllers/CommunicationController.cs
+++ src/EventStore.Core/Services/Transport/Http/Controllers/CommunicationController.cs
@@ -91,9 +91,9 @@ namespace EventStore.Core.Services.Transport.Http.Controllers
 
         protected static string MakeUrl(HttpEntityManager http, string path)
         {
-            var builder = new UriBuilder(http.ResponseUrl);
             if(path.Length > 0 && path[0] == '/') path = path.Substring(1);
-            builder.Path = builder.Path + path;
+            var hostUri = http.ResponseUrl;
+            var builder = new UriBuilder(hostUri.Scheme, hostUri.Host, hostUri.Port, hostUri.LocalPath + path);
             return builder.Uri.AbsoluteUri;
         }
     }
