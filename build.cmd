d9aef39898744c38aad1e7405c02729776ef57a1
shaan1337 <sniper111@gmail.com>
2018-08-20 11:15:45 +0400

    Add quick-with-ui to build.cmd help

---
+++ build.cmd
@@ -0,0 +1,17 @@
+@echo off
+
+if '%1'=='/?' goto help
+if '%1'=='/help' goto help
+if '%1'=='--help' goto help
+if '%1'=='-help' goto help
+if '%1'=='-h' goto help
+
+:: if ($psake.build_success -eq $false) { exit 1 } else { exit 0 }"
+
+powershell -NoProfile -ExecutionPolicy Bypass -Command "& '%~dp0\scripts\build-windows\build.ps1' %*;"
+exit /B %errorlevel%
+
+:help
+
+echo Usage:
+echo build.cmd ^quick^|quick-with-ui^|full^|clean-all^|v8^|js1^ ^[-Configuration=debug^|release^] ^[-Platform=x64^|x86^] ^[-Version=0.0.0.0^] ^[-SpecificVisualStudioVersion=Windows7.1SDK^|2013^|2012^|2010^] ^[-ForceNetwork^] ^[-Defines=^]
