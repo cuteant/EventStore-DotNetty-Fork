e2474fad95f6ab292d904be53cdbef507b08711b
James Nugent <james@jen20.com>
2018-09-03 09:47:39 -0500

    cleanup: Add cached bools for platform detection

--- src/EventStore.Common/Utils/Runtime.cs
+++ src/EventStore.Common/Utils/Runtime.cs
@@ -1,9 +1,14 @@
-﻿//using System;
+﻿using System;
 
-//namespace EventStore.Common.Utils
-//{
-//    public static class Runtime
-//    {
-//        public static readonly bool IsMono = Type.GetType("Mono.Runtime") != null;
-//    }
-//}
+namespace EventStore.Common.Utils
+{
+    public static class Runtime
+    {
+        public static readonly bool IsMono = Type.GetType("Mono.Runtime") != null;
+
+        public static readonly bool IsUnixOrMac = Environment.OSVersion.Platform == PlatformID.Unix
+                                                  | Environment.OSVersion.Platform == PlatformID.MacOSX;
+
+        public static readonly bool IsWindows = !IsUnixOrMac;
+    }
+}
