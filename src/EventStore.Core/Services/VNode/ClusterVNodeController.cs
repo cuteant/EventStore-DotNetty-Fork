wangzhefeng2000/EventStore

Add a new function DisableIdleDetection to QueueStatsCollector that waits for all queues to terminate before continuing by keeping track of number of queues started/stopped (_totalStarted) Call DisableIdleDetection() in OneTimeTearDown for all TestFixtures using QueueStatsCollector.InitializeIdleDetection() Rename _notifyLock to _notifyIdleLock
Make sure all QueuedHandlers are stopped even if _exitProcessOnShutdown = false

Add a variable _idleDetection to keep track whether idle detection was enabled during instance creation. This makes sure previous node instances do not have any impact on the idle detection mechanism.

src/EventStore.Core/Services/VNode/ClusterVNodeController.cs
@@ -407,17 +407,17 @@ namespace EventStore.Core.Services.VNode
             {
                 Log.ErrorException(exc, "Error when publishing {0}.", message);
             }
+            try
+            {
+                _node.WorkersHandler.Stop();
+                _mainQueue.RequestStop();
+            }
+            catch (Exception exc)
+            {
+                Log.ErrorException(exc, "Error when stopping workers/main queue.");
+            }
             if (_exitProcessOnShutdown)
             {
-                try
-                {
-                    _node.WorkersHandler.Stop();
-                    _mainQueue.RequestStop();
-                }
-                catch (Exception exc)
-                {
-                    Log.ErrorException(exc, "Error when stopping workers/main queue.");
-                }
                 Application.Exit(ExitCode.Success, "Shutdown and exit from process was requested.");
             }
         }

cuteant/EventStore-DotNetty-Fork

src/EventStore.Core/Services/VNode/ClusterVNodeController.cs
@@ -407,17 +407,17 @@ namespace EventStore.Core.Services.VNode
             {
                 Log.LogError(exc, "Error when publishing {0}.", message);
             }
+            try
+            {
+                _node.WorkersHandler.Stop();
+                _mainQueue.RequestStop();
+            }
+            catch (Exception exc)
+            {
+                Log.LogError(exc, "Error when stopping workers/main queue.");
+            }
             if (_exitProcessOnShutdown)
             {
-                try
-                {
-                    _node.WorkersHandler.Stop();
-                    _mainQueue.RequestStop();
-                }
-                catch (Exception exc)
-                {
-                    Log.LogError(exc, "Error when stopping workers/main queue.");
-                }
                 Application.Exit(ExitCode.Success, "Shutdown and exit from process was requested.");
             }
         }